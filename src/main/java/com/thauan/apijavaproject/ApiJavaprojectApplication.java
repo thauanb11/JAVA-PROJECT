9d6fb395ef986521734675156e617f937047f858
thauanb11 <guedesbruno40@gmail.com>
2026-07-06 22:37:15 -0300

    first commit

---
+++ src/main/java/com/thauan/apijavaproject/ApiJavaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.thauan.apijavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJavaprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thauan/apijavaproject/ApiJavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thauan.apijavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
