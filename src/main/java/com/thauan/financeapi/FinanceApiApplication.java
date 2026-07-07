709ee1988dc7460d8433e6b81bbba69034a35f68
thauanb11 <guedesbruno40@gmail.com>
2026-07-07 09:58:58 -0300

    chore: create initial project structure

--- src/main/java/com/thauan/financeapi/FinanceApiApplication.java
+++ src/main/java/com/thauan/financeapi/FinanceApiApplication.java
@@ -1,13 +1,13 @@
-package com.thauan.apijavaproject;
+package com.thauan.financeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiJavaprojectApplication {
+public class FinanceApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiJavaprojectApplication.class, args);
+        SpringApplication.run(FinanceApiApplication.class, args);
     }
 
 }
--- src/test/java/com/thauan/financeapi/ApiJavaprojectApplicationTests.java
+++ src/test/java/com/thauan/financeapi/ApiJavaprojectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thauan.apijavaproject;
+package com.thauan.financeapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
