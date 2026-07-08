b50342612984cf8618fa10c1db073c6d518477c3
thauanb11 <guedesbruno40@gmail.com>
2026-07-08 19:37:02 -0300

    feat: Healthcontroller code review

--- src/main/java/com/thauan/financeapi/controller/HealthController.java
+++ src/main/java/com/thauan/financeapi/controller/HealthController.java
@@ -1,4 +1,14 @@
 package com.thauan.financeapi.controller;
 
-public class HealthController {
+import com.thauan.financeapi.dto.HealthResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController   {
+
+    @GetMapping("/health")
+    public HealthResponse  health() {
+        return HealthResponse.up("Finance API", "1.0.0");
+    }
 }
